c9475bc842b15ca5f249c1f98df29a793653dfd5
Haddy Prawidya <haddyprawidya@gmail.com>
2023-07-15 10:43:43 +0800

    Change all contents

---
+++ main.js
@@ -0,0 +1,5 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar__menu");
+document.querySelector(".navbar__grid").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
